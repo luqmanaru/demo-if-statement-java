e4b2dff93e54aa3e086f091b899b7112721508ec
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-20 08:19:23 +0700

    Create demo-if-statement.java

---
+++ demo-if-statement.java
@@ -0,0 +1,19 @@
+package com.mycompany.strukturkontrolpemilihan;
+
+/**
+ * Demonstrasi Statement If Tunggal
+ * @author hp
+ * @contributor luqmanaru
+ */
+public class StrukturKontrolPemilihan {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int grade = 68;
+        
+        // Statement if untuk evaluasi nilai
+        if (grade > 60) {
+            System.out.println("Congratulations!");
+            System.out.println("You passed!");
+        }
+    }
+}
